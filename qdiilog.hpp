5696ef2f2b1e01a68f42bf6cb21d0178b8a50630
qdii <victor.lavaud@gmail.com>
2012-09-08 10:08:02 -0400

    Added documentation.

--- qdiilog.hpp
+++ qdiilog.hpp
@@ -1,6 +1,47 @@
 #ifndef QDIILOG_H
 #define QDIILOG_H
 
+/**@author qdii
+ *
+ * qdiilog is a header-only library that provides facilities to log messages
+ * for tracing, debugging, etc. The messages can be written on screen or on
+ * file.
+ * 
+ * 5 objects are available to the user to log messages, they are:
+ * - log_debug
+ * - log_trace
+ * - log_info
+ * - log_warning
+ * - log_error
+ * They work the same way std::cout does, for instance:
+ * log_warning << "foo() was passed a null pointer" << std::endl;
+ *
+ * The objects will all log to std::cout by default, but you can change this 
+ * behaviour by calling setOutput(). Actually, there are 2 setOutput functions. 
+ * One of them is a member function that you can call on any log object, like
+ * log_debug.setOutput( someOutput ), and the other one is a global function
+ * that will call setOutput on every available log object.
+ * Note that setOutput() takes a std::ostream reference as a parameter.
+ * 
+ * You can restrict the messages output to only the important ones by calling
+ * the global function setLogLevel() with one of the following parameters:
+ * - Loglevel::debug
+ * - Loglevel::trace
+ * - Loglevel::info
+ * - Loglevel::warning
+ * - Loglevel::error
+ * If you call setLogLevel( Loglevel::warning ), only the error and warning
+ * messages will be processed, the more detailed messages will be ignored.
+ * 
+ * A handy feature is the possibility to disable the logging easily:
+ * Instead of writing:
+ * if ( foo() != SUCCESS )
+ * {
+ *    log_warning << "problem!" << std::endl;
+ * }
+ * You can write:
+ * log_warning( foo() != SUCCESS ) << "problem" << std::endl;
+ */
 #include <iostream>
 
 #ifdef QDIILOG_NAMESPACE
