1d2a76ed74955987e6594b90d971b2079718f790
qdii <victor.lavaud@gmail.com>
2012-09-08 10:58:00 -0400

    Added possibility to prepend text to all messages. So that, for instance log_error.setPrependText("[EE] "); log_error << "an error occurred!" << std::endl;
    outputs:
    [EE] an error occurred!

--- qdiilog.hpp
+++ qdiilog.hpp
@@ -6,7 +6,7 @@
  * qdiilog is a header-only library that provides facilities to log messages
  * for tracing, debugging, etc. The messages can be written on screen or on
  * file.
- * 
+ *
  * 5 objects are available to the user to log messages, they are:
  * - log_debug
  * - log_trace
@@ -16,13 +16,13 @@
  * They work the same way std::cout does, for instance:
  * log_warning << "foo() was passed a null pointer" << std::endl;
  *
- * The objects will all log to std::cout by default, but you can change this 
- * behaviour by calling setOutput(). Actually, there are 2 setOutput functions. 
+ * The objects will all log to std::cout by default, but you can change this
+ * behaviour by calling setOutput(). Actually, there are 2 setOutput functions.
  * One of them is a member function that you can call on any log object, like
  * log_debug.setOutput( someOutput ), and the other one is a global function
  * that will call setOutput on every available log object.
  * Note that setOutput() takes a std::ostream reference as a parameter.
- * 
+ *
  * You can restrict the messages output to only the important ones by calling
  * the global function setLogLevel() with one of the following parameters:
  * - Loglevel::debug
@@ -32,7 +32,7 @@
  * - Loglevel::error
  * If you call setLogLevel( Loglevel::warning ), only the error and warning
  * messages will be processed, the more detailed messages will be ignored.
- * 
+ *
  * A handy feature is the possibility to disable the logging easily:
  * Instead of writing:
  * if ( foo() != SUCCESS )
@@ -43,6 +43,7 @@
  * log_warning( foo() != SUCCESS ) << "problem" << std::endl;
  */
 #include <iostream>
+#include <memory> // auto_ptr
 
 #ifdef QDIILOG_NAMESPACE
 namespace QDIILOG_NAMESPACE
@@ -67,29 +68,8 @@ static const ErrorCode          INVALID_LOGLEVEL    = -1;
 #define QDIILOG_DECL_HIDDEN __attribute__ ((visibility("hidden")))
 #define QDIILOG_DECL_VISIBLE __attribute__ ((visibility("default")))
 
-#ifdef QDIILOG_AS_A_DLL
-#   define QDIILOG_DECL QDIILOG_DECL_VISIBLE
-#else
-#   define QDIILOG_DECL QDIILOG_DECL_HIDDEN
-#endif // QDIILOG_AS_A_DLL
-
-template <typename QdiiLogParameters>
-ErrorCode qdiilog_init() QDIILOG_DECL_VISIBLE;
-
-template <typename QdiiLogParameters>
-ErrorCode qdiilog_end() QDIILOG_DECL_VISIBLE;
-
-#define QDIILOG_LOGGING_TYPE_NULL 0
-#define QDIILOG_LOGGING_TYPE_STDCOUT 1
-#define QDIILOG_LOGGING_TYPE_STDCERR 2
-#define QDIILOG_LOGGING_TYPE_FILE 3
-
-#ifndef QDIILOG_LOGGING_TYPE
-#   define QDIILOG_LOGGING_TYPE QDIILOG_LOGGING_TYPE_STDCOUT
-#endif
-
-
 //------------------------------------------------------------
+struct QDIILOG_DECL_HIDDEN QdiilogOstream;
 struct QdiilogOstream
 {
     typedef std::ostream Output;
@@ -103,20 +83,40 @@ struct QdiilogOstream
 
 //------------------------------------------------------------
 template <typename QdiilogParameters>
+struct UndecoratedLogger;
+
+//------------------------------------------------------------
+template <typename QdiilogParameters>
 struct Logger
 {
     typedef typename QdiilogParameters::Output Output;
 
-    Logger( Loglevel _level = Loglevel::error )
+    Logger( Loglevel _level = Loglevel::error, bool _notDecorated = false )
         :m_output( nullptr )
         ,m_level( _level )
+        ,m_userPrepend()
+        ,m_undecoratedLogger(nullptr)
     {
+        if (!_notDecorated)
+            m_undecoratedLogger = new UndecoratedLogger<QdiilogParameters>( _level );
     }
-    virtual ~Logger() { }
+    
+    virtual ~Logger() 
+    { 
+        delete m_undecoratedLogger;
+    }
 
     ErrorCode setOutput( Output & _output )
     {
         m_output = &_output;
+        if (m_undecoratedLogger)
+            m_undecoratedLogger->m_output = &_output;
+        return OK;
+    }
+
+    ErrorCode setPrependText( const std::string & _prependText )
+    {
+        m_userPrepend.assign( _prependText );            
         return OK;
     }
     Logger & operator()( bool _condition );
@@ -137,6 +137,38 @@ public:
     static void setLogLevel( Loglevel _level )
     {
         g_config.filter_level = _level;
+    }
+
+private:
+    virtual bool isDecorated() const
+    {
+        return m_userPrepend.size() > 0;
+    }
+    virtual void prepend()
+    {
+        if( m_userPrepend.size() && m_output )
+        {
+            ( *m_output ) << m_userPrepend;
+        }
+    }
+    std::string m_userPrepend;
+
+    UndecoratedLogger<QdiilogParameters> * m_undecoratedLogger;
+};
+
+//------------------------------------------------------------
+template <typename QdiilogParameters>
+struct UndecoratedLogger : public Logger<QdiilogParameters>
+{
+    UndecoratedLogger( Loglevel _level = Loglevel::error )
+        :Logger<QdiilogParameters>( _level, true )
+    {
+        
+    }
+    virtual void prepend() override final {};
+    bool isDecorated() const
+    {
+        return false;
     }
 };
 
@@ -169,36 +201,42 @@ Logger<QdiilogParameters> & operator<<( Logger<QdiilogParameters> & _logger, T&&
     if( _logger.m_output )
     {
         bool canLog = false;
-        switch(_logger.m_level)
+
+        switch( _logger.m_level )
         {
-            case Loglevel::error:
-                canLog |= (_logger.g_config.filter_level == Loglevel::error);
-                
-            case Loglevel::warning:
-                canLog |= (_logger.g_config.filter_level == Loglevel::warning);
-            
-            case Loglevel::info:
-                canLog |= (_logger.g_config.filter_level == Loglevel::info);
-                
-            case Loglevel::trace:
-                canLog |= (_logger.g_config.filter_level == Loglevel::trace);
-                
-            case Loglevel::debug:
-                canLog |= (_logger.g_config.filter_level == Loglevel::debug);
-                break;
+        case Loglevel::error:
+            canLog |= ( _logger.g_config.filter_level == Loglevel::error );
+
+        case Loglevel::warning:
+            canLog |= ( _logger.g_config.filter_level == Loglevel::warning );
+
+        case Loglevel::info:
+            canLog |= ( _logger.g_config.filter_level == Loglevel::info );
+
+        case Loglevel::trace:
+            canLog |= ( _logger.g_config.filter_level == Loglevel::trace );
+
+        case Loglevel::debug:
+            canLog |= ( _logger.g_config.filter_level == Loglevel::debug );
+            break;
         }
+
         if( canLog )
+        {
+            _logger.prepend();
             *( _logger.m_output ) << std::forward<T>( _t );
+        }
     }
 
-    return _logger;
+    return (_logger.isDecorated() && _logger.m_undecoratedLogger) ? 
+        *(_logger.m_undecoratedLogger) : _logger;
 }
 
 //------------------------------------------------------------
 template <typename QdiilogParameters>
 Logger<QdiilogParameters> & Logger<QdiilogParameters>::operator()( bool _condition )
 {
-    return _condition ? *this : OutputNull<QdiilogParameters>::nullOutput;;
+    return _condition ? *this : OutputNull<QdiilogParameters>::nullOutput;
 }
 
 //------------------------------------------------------------
@@ -220,15 +258,61 @@ inline
 ErrorCode setOutput( QdiilogOstream::Output & _output )
 {
     ErrorCode ret = log_debug.setOutput( _output );
-    if (ret == OK)
+
+    if( ret == OK )
         ret = log_trace.setOutput( _output );
-    if (ret == OK)
+
+    if( ret == OK )
         ret = log_info.setOutput( _output );
-    if (ret == OK)
+
+    if( ret == OK )
         ret = log_warning.setOutput( _output );
-    if (ret == OK)
+
+    if( ret == OK )
         ret = log_error.setOutput( _output );
-    
+
+    return ret;
+}
+
+//------------------------------------------------------------
+inline
+ErrorCode setPrependText( const std::string _prependText )
+{
+    ErrorCode ret = log_debug.setPrependText( _prependText );
+
+    if( ret == OK )
+        ret = log_trace.setPrependText( _prependText );
+
+    if( ret == OK )
+        ret = log_info.setPrependText( _prependText );
+
+    if( ret == OK )
+        ret = log_warning.setPrependText( _prependText );
+
+    if( ret == OK )
+        ret = log_error.setPrependText( _prependText );
+
+    return ret;
+}
+
+//------------------------------------------------------------
+inline
+ErrorCode setPrependTextQdiiFlavour()
+{
+    ErrorCode ret = log_debug.setPrependText( "" );
+
+    if( ret == OK )
+        ret = log_trace.setPrependText( "" );
+
+    if( ret == OK )
+        ret = log_info.setPrependText( "[..] " );
+
+    if( ret == OK )
+        ret = log_warning.setPrependText( "[ww] " );
+
+    if( ret == OK )
+        ret = log_error.setPrependText( "[EE] " );
+
     return ret;
 }
 
